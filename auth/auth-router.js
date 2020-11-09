d5c7ba869d2603ea751946e03c9f6ba6e306cc95
Kasech Isaac <kasechisaac@Kasechs-MacBook-Air.local>
2020-11-08 18:56:42 -0800

    done

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,63 @@
 const router = require('express').Router();
+const bcrypt = require("bcryptjs")
+const jwt=require("jsonwebtoken")
+const db = require("./auth-model")
 
-router.post('/register', (req, res) => {
+
+const {restrict} = require("./authenticate-middleware")
+
+
+router.post('/register', async (req, res) => {
   // implement registration
+  try{
+    const {username, password} = req.body
+    const users = await db.findByUser(username)
+    if (users) {
+        return res.status(409).json({
+            message: "Username is already taken",
+        })
+    }
+    const newUser =await db.add({
+        username,
+        password: await bcrypt.hash(password, 13),
+      
+    })
+      res.status(200).json(newUser)
+     }catch(err){
+       console.log(err)
+        }
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
   // implement login
+  try{
+    const { username, password} = req.body
+    const theUser = await db.findByUser({ username })
+
+    if (!theUser) {
+      return res.status(400).json({
+        message: "You shall not pass!",
+      });
+    }
+    const passwordCheck = await bcrypt.compare(password, theUser.password);
+
+        if (!passwordCheck) {
+      return res.status(400).json({
+        message: "You shall not pass!",
+      });
+    }
+    
+    const token=jwt.sign({
+  userId: user.id,
+  user:user.username,
+    },process.env.JWT_SECRET)
+    
+    res.cookie("token", token) 
+
+    res.status(200).json({ message: `Welcome ${theUser.username}` })
+  }catch(err){
+    next(err)
+    }
 });
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,31 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require("jsonwebtoken")
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+module.exports ={
+  restrict
+} 
+function restrict(){
+return async (req, res, next) => {
+try{
+  const token = req.cookies.token
+  if(!token){
+    return res.status(401).json({ message: 'shall not pass!' });
+  }
+
+  jwt.verify(token,process.env.JWT_SECRET, (err, decoded)=>{
+    if(err){
+      return res.status(401).json({
+      message:"Invalid credentials",
+          })	
+      }
+      req.token = decoded
+
+      next()
+  })
+}catch(err){
+            next(err)
+        }
 };
+}
